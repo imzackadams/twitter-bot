292adbbbc7feb9ac34112d95c1bd8613ee4a3fa9
Zack Adams <imzackadams@protonmail.com>
2022-06-02 04:38:35 -0400

    fix: incorrect var name :D

--- bot.py
+++ bot.py
@@ -3,9 +3,8 @@ from tweepy import StreamRule
 import time
 import os
 from dotenv import load_dotenv, find_dotenv
+
 load_dotenv(find_dotenv())
-
-
 
 client = tweepy.Client(consumer_key=os.environ.get("API_KEY"),
                        consumer_secret=os.environ.get("API_KEY_SECRET"),
@@ -18,13 +17,15 @@ class TweetPrinterV2(tweepy.StreamingClient):
     def on_tweet(self, tweet):
         print(f"{tweet.id} {tweet.created_at} ({tweet.author_id}): {tweet.text}")
         print("-" * 50)
-        time.sleep(45)
+        time.sleep(30)
+        response = client.retweet(tweet.id)
+        print(response)
 
-        try:
-            response = client.retweet(tweet.id)
-            print(response)
-        except:
-            print(f"Failed to retweet {tweet.id}")
+        # try:
+        #     response = client.retweet(tweet.id)
+        #     print(response)
+        # except:
+        #     print(f"Failed to retweet {tweet.id}")
 
 
 printer = TweetPrinterV2(os.environ.get("BEARER_TOKEN"), wait_on_rate_limit=True)
